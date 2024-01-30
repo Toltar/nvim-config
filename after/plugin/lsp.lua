975d3c1fc3221e0e1eaebbe53b403ea96a55c8fb
Toltar <matthew@matthew-dickens.com>
2024-01-29 17:57:32 -0800

    Implemented LSP!

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,23 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+  
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+	  'tsserver', 
+	  'eslint', 
+	  'rust_analyzer',
+  } 
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
--- lua/mattcodes/packer.lua
+++ lua/mattcodes/packer.lua
@@ -26,4 +26,22 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
+
+  -- LSP Manager
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+  }
 end)
