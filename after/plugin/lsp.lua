26fc264e0ca4259675fb3ca563587bfb11b9c044
Toltar <matthew@matthew-dickens.com>
2024-01-29 18:51:01 -0800

    Removed unessesary config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,6 @@ lsp_zero.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 end)
 
 
