eff6a9146b319de50b2293a30bb80cfa23de629c
Toltar <matthew@matthew-dickens.com>
2024-02-02 14:44:18 -0800

    Implemented more settings and also deleted the hello world file

--- lua/mattcodes/init.lua
+++
@@ -1,1 +0,0 @@
-print("hello from the mattcodes directory")
--- lua/mattcodes/set.lua
+++ lua/mattcodes/set.lua
@@ -1,11 +1,23 @@
+-- Setting Numbers and Relative Line numbers here
+-- ==============================================
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+
+-- SPACING AND INDENTATION
+-- =======================
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
 
+
+-- Other Settings
+-- =================
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
