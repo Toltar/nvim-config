55a4193bed3b6d5ab5b0157e64ae5249d2158a18
Toltar <matthew@matthew-dickens.com>
2024-02-05 12:00:23 -0800

    Implemented better mapping for window splitting and moving between splits

--- lua/mattcodes/remap.lua
+++ lua/mattcodes/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Easy split windows and quit windows splits with leader key instead of Ctrl as a pereference
+vim.keymap.set("n", "<leader>wn", vim.cmd.split)
+vim.keymap.set("n", "<leader>wv", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>wq", vim.cmd.close)
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+
