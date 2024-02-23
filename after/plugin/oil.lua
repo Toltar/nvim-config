b92a819c3bced32d2f1021483f7dcd8ff0bec157
Toltar <matthew@matthewdickens.com>
2024-02-23 14:16:04 -0800

    Formated oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,16 +1,14 @@
-local oil = require("oil")
+local oil = require 'oil'
 
-oil.setup({
-    default_file_explorer = true,
-    keymaps = {
-        ["<C-o>"] = "actions.preview",
-    },
-    experimental_watch_for_changes = true,
-    view_options = {
-        show_hidden = true,
-    },
-})
+oil.setup {
+  default_file_explorer = true,
+  keymaps = {
+    ['<C-o>'] = 'actions.preview',
+  },
+  experimental_watch_for_changes = true,
+  view_options = {
+    show_hidden = true,
+  },
+}
 
-
-
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
