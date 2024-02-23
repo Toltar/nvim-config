1af0add6f65eae0e251a68d420ca959690388e4d
Toltar <matthew@matthewdickens.com>
2024-02-23 09:59:20 -0800

    Enable oil hidden file support

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,1 +1,15 @@
+local oil = require("oil")
+
+oil.setup({
+    default_file_explorer = true,
+    keymaps = {
+        ["<C-o>"] = "actions.preview",
+    },
+    view_options = {
+        show_hidden = true,
+    },
+})
+
+
+
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
