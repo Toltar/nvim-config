a30c4fdd25cedfcf769039dbce52e477f28ef85b
Toltar <matthew@matthewdickens.com>
2024-02-22 18:34:39 -0800

    Added neovim installation to the installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,3 @@
 yes | sudo apt install ripgrep lus5.3 git build-essential cmake python3.10
 
-
-git clonse --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+yes | sudo apt install neovim
