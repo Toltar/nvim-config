16a960d1ebcc73dfbd331cdb5b997f03433e4f44
Toltar <matthew@matthewdickens.com>
2024-02-23 14:15:19 -0800

    Format on post and not pre

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -39,7 +39,7 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 augroup('__formatter__', { clear = true })
-autocmd('BufWritePre', {
+autocmd('BufWritePost', {
   group = '__formatter__',
   command = ':FormatWrite',
 })
