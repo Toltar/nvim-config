666737206541c114454998a8708869c775908815
Toltar <matthew@matthewdickens.com>
2024-02-25 14:55:24 -0800

    Remove autocmd since it does not work in my wsl instance

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -10,13 +10,13 @@ lint.linters_by_ft = {
     yaml = { 'yamllint', 'actionlint' },
 }
 
-local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-    group = lint_augroup,
-    callback = function()
-        lint.try_lint()
-    end,
-})
+-- local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+-- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+--    group = lint_augroup,
+--    callback = function()
+--        lint.try_lint()
+--    end,
+-- })
 
 vim.keymap.set('n', '<leader>ll', function()
     lint.try_lint()
