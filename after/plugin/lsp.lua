8f0d850f85820266b415b82ec032b70c5ef5016c
Toltar <matthew@matthewdickens.com>
2024-08-19 15:14:33 -0700

    Ensured zig language server is installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,44 +4,45 @@ local lspconfig = require 'lspconfig'
 lspconfig.intelephense.setup {}
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps { buffer = bufnr }
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps { buffer = bufnr }
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup {}
 require('mason-lspconfig').setup {
-    ensure_installed = {
-        'tsserver',
-        'eslint',
-        'rust_analyzer',
-    },
-    handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-            require('lspconfig').lua_ls.setup {
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            globals = {
-                                'vim',
-                                'require',
-                            },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file('', true),
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            }
-        end,
-    },
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+    'zls',     -- zig
+  },
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      require('lspconfig').lua_ls.setup {
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              globals = {
+                'vim',
+                'require',
+              },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file('', true),
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }
+    end,
+  },
 }
