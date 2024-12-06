e632ab850c99367c3c449e3631e6b6e0e5f620bb
Toltar <matthew@matthewdickens.com>
2024-12-05 17:04:08 -0800

    Fix lsp errors when booting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,7 +14,9 @@ end)
 require('mason').setup {}
 require('mason-lspconfig').setup {
   ensure_installed = {
-    'tsserver',
+    'ts_ls',
+    'eslint',
+    'sqlls',
     'gopls',
     'eslint',
     'rust_analyzer',
