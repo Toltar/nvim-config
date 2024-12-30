10ff9146d0e9d956f32e43a9d48909faa2beb1d9
Toltar <matthew@matthewdickens.com>
2024-12-29 16:45:52 -0800

    Add blankline and debugging support

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,49 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim'
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    require('mason-nvim-dap').setup {
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {
+        'delve',
+        'bash-debug-adapter',
+        'chrome-debug-adapter',
+        'codelldb',
+        'debugpy',
+        'js-debug-adapter',
+      },   
+    }
+    dapui.setup {
+      -- Set icons to characters that are more likely to work in every terminal.
+      --    Feel free to remove or use ones that you like more! :)
+      --    Don't feel like these are good choices.
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  end,
+
+}
